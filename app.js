569c96c0b236ddb6b978c3546e9ccee8a9be6699
vandergj <89868874+vandergj@users.noreply.github.com>
2022-01-14 18:26:36 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,4 @@
+let prompter = prompt("What is your name?");
+let prompter2 = prompt("What is your favorite color?");
+document.getElementById('replace-me').innerText=prompter;
+document.getElementById('replace-me-too').innerText=prompter2;
