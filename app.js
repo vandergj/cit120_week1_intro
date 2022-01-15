544a7410c8737b37a666b9782f5188fd315a5908
vandergj <89868874+vandergj@users.noreply.github.com>
2022-01-14 22:29:56 -0500

    update

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
-let prompter = prompt("What is your name?");
-
-document.getElementById('replace-me').innerText=prompter;
+let input_name = prompt("What is your name?");
+let input_color = prompt("What is your favorite color?");
+document.getElementById('replace-me').innerText= "Hello my name is " + input_name + " and my favorite color is " + input_color;
