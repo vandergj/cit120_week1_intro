cbc86f843e5c9bb7ba44969e7cb9a99f4ade40c5
vandergj <vandergj@iu.edu>
2022-01-19 13:16:36 -0500

    modify index.html

---
+++ app.js
@@ -0,0 +1,4 @@
+let input_name = prompt("What is your name?");
+let input_color = prompt("What is your favorite color?");
+
+document.getElementById('1').innerText = "Hello my name is " + input_name + " and my favorite color is " + input_color;
